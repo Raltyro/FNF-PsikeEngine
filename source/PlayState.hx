45884c2cc30d20201deda3da0d5ce23284c625df
Raltyro <78720179+Raltyro@users.noreply.github.com>
2022-06-18 20:57:34 +0700

    oopsie idk when did i revert this one im sorry

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2894,6 +2894,9 @@ class PlayState extends MusicBeatState
 
 		super.update(elapsed);
 
+		setOnLuas('curDecStep', curDecStep);
+		setOnLuas('curDecBeat', curDecBeat);
+
 		scoreTxt.text = 'Score: ' + songScore + ' | Misses: ' + songMisses + ' | Rating: ' + ratingName;
 		if(ratingName != '?')
 			scoreTxt.text += ' (' + Highscore.floorDecimal(ratingPercent * 100, 2) + '%)' + ' - ' + ratingFC;
