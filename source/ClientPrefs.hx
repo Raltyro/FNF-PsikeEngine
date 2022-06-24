1ef7a356730c5171445acfb0cd206b0b2fbe49ba
Raltyro <78720179+Raltyro@users.noreply.github.com>
2022-06-24 12:43:53 +0700

    fffff

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -4,6 +4,7 @@ import flixel.FlxG;
 import flixel.util.FlxSave;
 import flixel.input.keyboard.FlxKey;
 import flixel.graphics.FlxGraphic;
+import lime.app.Application;
 import Controls;
 
 class ClientPrefs {
